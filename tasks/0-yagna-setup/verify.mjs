ab2ed4911a44f8f0e4c6614f235d6b3aee4ced62
Komal Vaghela <komalv.brainerhub@gmail.com>
2023-04-05 14:06:59 +0530

    Setup yogana in local machine

--- tasks/0-yagna-setup/verify.mjs
+++ tasks/0-yagna-setup/verify.mjs
@@ -1,10 +1,24 @@
 import { TaskExecutor } from "yajsapi";
 import { writeFileSync } from 'fs'
 
+const executorOptions = {
+  package: '9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae',
+  yagnaOptions: { apiKey: '677c7789f69343edaaea3f6795b8653f' }
+};
+
+
 (async function main() {
-  const executor = await TaskExecutor.create("9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae");
+  const executor = await TaskExecutor.create(
+    executorOptions
+  );
   await executor.run(async (ctx) => {
-    writeFileSync('result.json', JSON.stringify({ts: Date.now(), token: (await ctx.run(`echo '${JSON.stringify({provider: ctx.provider, activity: {id: ctx.activity.agreementId, ts: Date.now(), options: ctx.activity.options.yagnaOptions}})}' | base64`)).stdout}))
+    writeFileSync('result.json', JSON.stringify({
+      ts: Date.now(),
+      token: (await ctx.run(`echo '${JSON.stringify({
+        provider: ctx.provider,
+        activity: { id: ctx.activity.agreementId, ts: Date.now(), options: ctx.activity.options.yagnaOptions }
+      })}' | base64`)).stdout
+    }))
   });
   await executor.end();
 })();
